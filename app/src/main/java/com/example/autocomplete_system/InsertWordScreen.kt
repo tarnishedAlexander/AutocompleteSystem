3b85457b9cf3976fa335caaa57a4bd3ae3deaa3f
mxrixn <126999395+mxrixn@users.noreply.github.com>
2023-11-04 12:44:51 -0400

    Pantalla principal donde se realiza la busqueda, pero sin la funcion del edit text

---
+++ app/src/main/java/com/example/autocomplete_system/InsertWordScreen.kt
@@ -0,0 +1,11 @@
+package com.example.autocomplete_system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InsertWordScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_word_screen)
+    }
+}
