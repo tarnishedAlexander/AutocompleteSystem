486f9638bc7d84365574d7a053c35d0b087a7a47
tarnishedAlexander <alex4750384@gmail.com>
2023-11-04 12:51:36 +0000

    commit innecesario

--- app/src/main/java/com/example/autocomplete_system/MainActivity.kt
+++ app/src/main/java/com/example/autocomplete_system/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.autocomplete_system
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
