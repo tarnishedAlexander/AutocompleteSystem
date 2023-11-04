b80ca67c7614eb9f5ad72b40a2b342c6323382d4
tarnishedAlexander <alex4750384@gmail.com>
2023-11-04 17:34:29 +0000

    working?

--- app/src/main/java/com/example/autocomplete_system/MainActivity.kt
+++ app/src/main/java/com/example/autocomplete_system/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.autocomplete_system
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trie: Trie
+    private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trie = Trie()
+        trie.insertWord("Hello world")
+        trie.insertWord("world")
+        trie.insertWord("hola")
+        trie.insertWord("paul")
+        trie.insertWord("paul")
+        trie.insertWord("pato")
+        trie.insertWord("partido")
+        trie.insertWord("patio")
+        trie.insertWord("pollo")
+
+
+        val wordsList = mutableListOf<String>() // Obteniendo las palabras del trie
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView)
+        adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, wordsList)
+        autoCompleteTextView.setAdapter(adapter)
+
+        autoCompleteTextView.threshold = 0 //mostrar el menu desplegable cuando ingresamos el primer prefijo
+
+        autoCompleteTextView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // not util
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // not util
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val prefix: String = s.toString()
+
+                //sugerencias del trie
+                val autoCompleteResults = trie.autoComplete(prefix)
+
+                //mostrar las sugerencias
+                adapter.clear()
+                adapter.addAll(autoCompleteResults)
+                adapter.notifyDataSetChanged()
+            }
+        })
     }
 }
