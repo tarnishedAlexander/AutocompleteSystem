942243091c2f7022aa7c1e4a57d65e5975082bd3
mxrixn <126999395+mxrixn@users.noreply.github.com>
2023-11-04 20:35:14 -0400

    diseño y recycler view sin terminar

---
+++ app/src/main/java/com/example/autocomplete_system/Adapters/LastWordPhraseSearchedAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.autocomplete_system.Adapters
+
+class LastWordPhraseSearchedAdapter : RecyclerView.Adapter<LastWordPhraseSearchedAdapter.LastWordPhraseSearchedViewHolder>() {
+
+    private var context: Context? = null
+    private var listOfWordsOrPhrases = mutableListOf<DataClaseWordsOrPhrases>()
+}
---
+++ app/src/main/java/com/example/autocomplete_system/DataClases/LastWordsOrPhrasesDB.kt
@@ -0,0 +1,4 @@
+package com.example.autocomplete_system.DataClases
+
+class LastWordsOrPhrasesDB {
+}
