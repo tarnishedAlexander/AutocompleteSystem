8831dcfbbac45871b40ca9ab8d2af29e78a3880d
tarnishedAlexander <alex4750384@gmail.com>
2023-11-06 06:25:30 +0000

    comment

--- app/src/main/java/com/example/autocomplete_system/Trie.kt
+++ app/src/main/java/com/example/autocomplete_system/Trie.kt
@@ -1,13 +1,15 @@
 package com.example.autocomplete_system
 
 class Trie {
-    private var root : TrieNode = TrieNode()
+    private var root: TrieNode = TrieNode()
 
     fun insertWord(word: String) {
         var currentNode: TrieNode = root
         //Recorre toda la palabra
         for (i in 0 until word.length) {
-            val character: Int = if (word[i] == ' ') 26 else word[i].toLowerCase() - 'a' //le da un indice al espacio o convierte a minúsculas y calcula su indice
+            //accede al indice de los caracteres, agregando un indice al espacio
+            val character: Int =
+                if (word[i] == ' ') 26 else word[i].toLowerCase() - 'a' //le da un indice al espacio o convierte a minúsculas y calcula su indice
 
             //Si en el carácter de la palabra dentro de la casilla del array es null, lo agrega
             if (currentNode.children[character] == null) {
@@ -23,50 +25,42 @@ class Trie {
         currentNode.isWord = true
     }
 
-
-
-    fun searchWord(word: String):Boolean{
-        var currentNode:TrieNode = root
-        //recorre la palabra
-        for(i in 0 until word.length){
-            val character:Int = word[i] - 'a'
-            //con el indice de la palabra busca la palabra
-            //si es null significa que no hay palabra
-            if(currentNode.children[character] == null){
-                return false
-            }
-            //apunta al siguiente caracter
-            currentNode = currentNode.children[character]!!
-        }
-        println(currentNode.currentCharacter)
-        return currentNode.isWord
-    }
-
     //busca la palabra con los prefijos
-    fun autoComplete(prefix:String):List<String>{
+    fun autoComplete(prefix: String): List<String> {
+        //aca se guardaran los resultados
         val result: MutableList<String> = mutableListOf()
         var currentNode: TrieNode? = root
-        for(i in 0 until prefix.length){
+
+        for (i in 0 until prefix.length) {
+            //accede al indice de los caracteres, agregando un indice al espacio
             val character: Int = if (prefix[i] == ' ') 26 else prefix[i].toLowerCase() - 'a'
-            if(currentNode?.children?.get(character) == null){
+            //si el nodo actual ya no tiene hijos entonces no retorna nada ya que deja de coincidir
+            if (currentNode?.children?.get(character) == null) {
                 return result
             }
+            //el cambio de puntero al nodo actual
             currentNode = currentNode.children[character]
         }
-        //dfs para buscar por fila de nodos encontrando todas los caracteres que puede matchear
-        fun dfs(node:TrieNode, word: String){
-            if(node.isWord){
-                result.add(prefix+word)
-            }
+        //llamando a la funcion dfs para encontrar el matcheo, pero verificando que no sea null(let), it es una referencia al nodo no null
+        currentNode?.let { dfs(it, prefix, result) }
+        return result
+    }
 
-            for(i in 0 until 27){
-                val childNode = node.children[i]
-                if(childNode != null){
-                    dfs(childNode, word + childNode.currentCharacter)
+    //dfs para matchear palabras
+    fun dfs(node: TrieNode?, prefix: String, result: MutableList<String>) {
+        //verifica que no sea null
+        node?.let {
+            //el caracter se va agregando a la lista
+            if (it.isWord) {
+                result.add(prefix)
+            }
+            //dfs como tal
+            for (i in 0 until 27) {
+                val childNode = it.children[i]
+                if (childNode != null) {
+                    dfs(childNode, prefix + childNode.currentCharacter, result)
                 }
             }
         }
-        currentNode?.let { dfs(it, "") }
-        return result
     }
 }
